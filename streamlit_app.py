738c989d3dd8dfdc28e09dbaa7a542fa714b4beb
Siddardha Nelapudi <156110982+Siddardha5@users.noreply.github.com>
2024-10-21 00:03:01 -0400

    v4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,23 @@
-import torch
-import streamlit as st
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import streamlit as st #for web dev
+from aitextgen import aitextgen #for ai text gen
 
-st.title('Natural Language Generation with GPT-2')
-st.markdown("A [simple demonstration](https://github.com/CaliberAI/streamlit-get-stories-aylien) of using [Streamlit](https://streamlit.io/) with [HuggingFace's GPT-2](https://github.com/huggingface/transformers/).")
+st.title("AITEXTGEN Web App")
 
-seed = st.text_input('Seed', 'The dog jumped')
-num_return_sequences = st.number_input('Number of generated sequences', 1, 100, 20)
-max_length = st.number_input('Length of sequences', 5, 100, 20)
-go = st.button('Generate')
+# instantiate the model / download
+ai = aitextgen()
 
-if go:
-    try:
-        model = GPT2LMHeadModel.from_pretrained('gpt2')
-        tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-        input_ids = torch.tensor(tokenizer.encode(seed)).unsqueeze(0)
-        output = model.generate(input_ids=input_ids,
-                                max_length=max_length,
-                                num_return_sequences=num_return_sequences,
-                                do_sample=True,
-                                length_penalty=10)
-        sequences = []
-        for j in range(len(output)):
-          for i in range(len(output[j])):
-              sequences.append(tokenizer.decode(output[j][i].tolist(), skip_special_tokens=True))
-        st.dataframe(sequences)
-    except Exception as e:
-        st.exception("Exception: %s\n" % e)
+# create a prompt text for the text generation 
+#prompt_text = "Python is awesome"
+prompt_text = st.text_input(label = "Enter your prompt text...",
+            value = "Computer is beautiful")
 
-st.markdown('___')
-st.markdown('by [CaliberAI](https://github.com/CaliberAI/)')
+with st.spinner("AI is at Work........"):
+    # text generation
+    gpt_text = ai.generate_one(prompt=prompt_text,
+            max_length = 100 )
+st.success("AI Successfully generated the below text ")
+st.balloons()
+# print ai generated text
+print(gpt_text)
+
+st.text(gpt_text)
